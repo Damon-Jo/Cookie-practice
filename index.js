b826c075184cc9f2ac4de1ab72d32dc409c9250c
Damon-Jo <lunaticjky@gmail.com>
2023-08-16 18:33:03 -0700

    created get request API to read a cookie

--- index.js
+++ index.js
@@ -4,15 +4,23 @@ const shelterRoutes = require('./routes/shelters');
 const dogRoutes = require('./routes/dogs');
 const adminRoutes = require('./routes/admin');
 
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 app.use('/shelters', shelterRoutes);
 app.use('/dogs', dogRoutes);
 app.use('/admin', adminRoutes);
 
 
-// this example route code is for cookies
+// the below example route codes are for cookies
+app.get('/greet', (req, res) => {
+    const { name = 'No-name' } = req.cookies; // code to read a cookie
+    res.send(`Hey there, ${name}!`);
+});
+
 app.get('/setname', (req, res) => {
-    res.cookie('name', 'stevie chicks');
-    res.cookie('animal', 'harlequin shrimp');
+    res.cookie('name', 'stevie chicks'); // code to set a cookie
+    res.cookie('animal', 'harlequin shrimp'); // code to set a cookie
     res.send('OK SENT YOU A COOKIE');
 });
 
